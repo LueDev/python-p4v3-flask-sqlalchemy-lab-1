c4e3b97cfcf6cfc35f230c55496cb5b556194381
PapiLuee <41023873+LueDev@users.noreply.github.com>
2024-05-23 20:04:12 -0400

    Very cool to play around with the server more. After using Express, a lot of this server work feels familiar and easy to grasp.

--- server/app.py
+++ server/app.py
@@ -1,9 +1,7 @@
-# server/app.py
 #!/usr/bin/env python3
 
-from flask import Flask, make_response
+from flask import Flask, make_response, jsonify
 from flask_migrate import Migrate
-
 from models import db, Earthquake
 
 app = Flask(__name__)
@@ -14,14 +12,34 @@ app.json.compact = False
 migrate = Migrate(app, db)
 db.init_app(app)
 
-
 @app.route('/')
 def index():
     body = {'message': 'Flask SQLAlchemy Lab 1'}
     return make_response(body, 200)
 
-# Add views here
+@app.route('/earthquakes/magnitude/<float:magnitude>')
+def get_earthquake_by_magnitude(magnitude):
+    earthquakes = Earthquake.query.filter(Earthquake.magnitude >= magnitude).all()
+    if earthquakes:
+        body = {"count": len(earthquakes), "quakes": [earthquake.to_dict() for earthquake in earthquakes]}
+        status = 200
+    else:
+        body = {"count": 0, "quakes": []}
+        status = 200
 
+    return make_response(jsonify(body), status)
+
+@app.route('/earthquakes/<int:id>')
+def get_earthquake_by_id(id):
+    earthquake = Earthquake.query.filter(Earthquake.id == id).first()
+    if earthquake:
+        body = earthquake.to_dict()
+        status = 200
+    else:
+        body = {'message': f'Earthquake {id} not found.'}
+        status = 404
+
+    return make_response(jsonify(body), status)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
